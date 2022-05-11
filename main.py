7f7f2c7debcdadf281be031b8112c0981882170a
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2022-05-12 05:21:25 +0800

    增加命令行控制

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+import click
 from PIL import Image
-from calendar import generate as calendar_generate
-from wallpapaer import generate as wallpaper_generate
+from calendar import generate
+from wallpapaer import combine
+
 from config import load_config
 
 
-config = load_config(config_path='example/asm.toml')
-calendar_image = calendar_generate(config, transparent=False)
-calendar_image.save('output/'+config['calendar']['output'], 'png')
+@click.group()
+def cli():
+    pass
 
-calendar_image = calendar_generate(config, transparent=True)
-wallpaper_image = Image.open('images/wallpaper.png')
-wallpaper_image = wallpaper_generate(calendar_image, wallpaper_image, config)
-wallpaper_image.save('output/'+config['wallpaper']['output'], 'png')
+
+@cli.command()
+@click.option('-c', '--config', default='config.toml', help='配置文件路径')
+def calendar(config):
+    config = load_config(config_path=config)
+    calendar_image = generate(config, transparent=False)
+    calendar_image.save(config['calendar']['output'], 'png')
+
+
+@cli.command()
+@click.option('-c', '--config', default='config.toml', help='配置文件路径')
+def wallpaper(config):
+    config = load_config(config_path=config)
+    calendar_image = generate(config, transparent=True)
+    wallpaper_image = Image.open(config['wallpaper']['input'])
+    wallpaper_image = combine(calendar_image, wallpaper_image,
+                              config['wallpaper']['code_zoom'],
+                              config['wallpaper']['code_pos'])
+    wallpaper_image.save(config['wallpaper']['output'], 'png')
+
+
+if __name__ == '__main__':
+    cli()
