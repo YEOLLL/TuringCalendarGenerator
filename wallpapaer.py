2d61be105925b35d0a78baca2ac924618c84e807
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2022-05-12 05:21:06 +0800

    修改传参，不传递整个 config

--- wallpapaer.py
+++ wallpapaer.py
@@ -1,15 +1,15 @@
 from PIL import Image
 
 
-def generate(calendar_image, wallpaper_image, config):
+def combine(calendar_image, wallpaper_image, zoom, pos):
     wallpaper_image = wallpaper_image.convert('RGBA')
 
-    width = int(calendar_image.size[0] * config['wallpaper']['code_zoom'])
-    height = int(calendar_image.size[1] * config['wallpaper']['code_zoom'])
+    width = int(calendar_image.size[0] * zoom)
+    height = int(calendar_image.size[1] * zoom)
     calendar_image = calendar_image.resize((width, height))
 
     temp_image = Image.new('RGBA', wallpaper_image.size, (255, 255, 255, 0))
-    temp_image.paste(calendar_image, config['wallpaper']['code_pos'])
+    temp_image.paste(calendar_image, pos)
 
     wallpaper_image.alpha_composite(temp_image)
     return wallpaper_image
