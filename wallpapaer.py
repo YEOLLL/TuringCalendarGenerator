3883883c1aee0bf28bad7843bb255c3517db8f72
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2022-05-11 13:23:06 +0800

    返回 PIL.Image

--- wallpapaer.py
+++ wallpapaer.py
@@ -7,7 +7,9 @@ def generate(calendar_image, wallpaper_image, config):
     width = int(calendar_image.size[0] * config['wallpaper']['code_zoom'])
     height = int(calendar_image.size[1] * config['wallpaper']['code_zoom'])
     calendar_image = calendar_image.resize((width, height))
+
     temp_image = Image.new('RGBA', wallpaper_image.size, (255, 255, 255, 0))
     temp_image.paste(calendar_image, config['wallpaper']['code_pos'])
+
     wallpaper_image.alpha_composite(temp_image)
-    wallpaper_image.save('output.png', 'png')
+    return wallpaper_image
