56f8adf3827cbd0ab7ada8ffd5914d5ec0c358ca
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2022-05-11 13:45:17 +0800

    合并壁纸

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from wallpapaer import generate as wallpaper_generate
 from config import load_config
 
 
-config = load_config(config_path='config.toml')
+config = load_config(config_path='example/asm.toml')
+calendar_image = calendar_generate(config, transparent=False)
+calendar_image.save('output/'+config['calendar']['output'], 'png')
+
 calendar_image = calendar_generate(config, transparent=True)
-calendar_image.save('output.png', 'png')
-wallpaper_image = Image.open('images/mi3_solarized1_16x9.jpg')
+wallpaper_image = Image.open('images/wallpaper.png')
 wallpaper_image = wallpaper_generate(calendar_image, wallpaper_image, config)
-# wallpaper_image.save(filename, 'png')
+wallpaper_image.save('output/'+config['wallpaper']['output'], 'png')
