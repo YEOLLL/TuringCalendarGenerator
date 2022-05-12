9657c9b475b60e7eea91f3e4b5fc353a0687f5db
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2022-05-12 09:41:40 +0800

    命名更改

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def wallpaper(config):
     calendar_image = generate(config, transparent=True)
     wallpaper_image = Image.open(config['wallpaper']['input'])
     wallpaper_image = combine(calendar_image, wallpaper_image,
-                              config['wallpaper']['code_zoom'],
-                              config['wallpaper']['code_pos'])
+                              config['wallpaper']['zoom'],
+                              config['wallpaper']['pos'])
     wallpaper_image.save(config['wallpaper']['output'], 'png')
     click.echo(f"壁纸已保存至 {config['wallpaper']['output']}")
 
