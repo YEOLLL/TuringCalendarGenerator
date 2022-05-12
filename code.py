51e4ee6eb18c89066aefe9e6c309c9fcdbe1d7ee
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2022-05-13 00:35:09 +0800

    更换亮色支持更好的主题，仿照自 OneHalfLight

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ from pygments.lexers import guess_lexer, get_lexer_by_name
 from pygments.formatters import ImageFormatter
 from pygments.style import Style
 from pygments.token import Keyword, Name, Comment, String, \
-    Number, Operator, Punctuation, Token
+    Number, Operator, Punctuation, Token, Text
 
 
 # 修改自 style/onedark.py
@@ -14,22 +14,22 @@ class OneDarkStyle(Style):
     background_color = '#00000000'  # 背景透明
     # background_color = '#ffffff'
     styles = {
-        Token: '#ABB2BF',
+        Token:                  '#ABB2BF',
 
-        Punctuation: '#ABB2BF',
-        Punctuation.Marker: '#ABB2BF',
+        Punctuation:            '#ABB2BF',
+        Punctuation.Marker:     '#ABB2BF',
 
-        Keyword: '#C678DD',
-        Keyword.Constant: '#E5C07B',
-        Keyword.Declaration: '#C678DD',
-        Keyword.Namespace: '#C678DD',
-        Keyword.Reserved: '#C678DD',
-        Keyword.Type: '#E5C07B',
+        Keyword:                '#C678DD',
+        Keyword.Constant:       '#E5C07B',
+        Keyword.Declaration:    '#C678DD',
+        Keyword.Namespace:      '#C678DD',
+        Keyword.Reserved:       '#C678DD',
+        Keyword.Type:           '#E5C07B',
 
-        Name: '#E06C75',
-        Name.Attribute: '#E06C75',
-        Name.Builtin: '#E5C07B',
-        Name.Class: '#E5C07B',
+        Name:                   '#E06C75',
+        Name.Attribute:         '#E06C75',
+        Name.Builtin:           '#E5C07B',
+        Name.Class:             '#E5C07B',
         Name.Function: '#61AFEF',  # 去除粗体
         Name.Function.Magic: '#56B6C2',  # 去除粗体
         Name.Other: '#E06C75',
@@ -47,7 +47,28 @@ class OneDarkStyle(Style):
     }
 
 
-# 生成高亮代码图
+class OneHalfLightStyle(Style):
+    default_style = ''
+    background_color = '#00000000'
+    styles = {
+        Comment: '#A0A1A7',
+
+        Text: "#383A42",
+
+        Keyword: '#A626A4',
+        Keyword.Constant: '#C18401',
+
+        Name.Variable: '#E45649',
+        Name.Function: '#0184BC',
+        Name.Class: '#C18401',
+
+        String: '#50A14F',
+
+        Number.Integer: '#C18401',
+        Number.Float: '#C18401',
+    }
+
+
 def highlight_image(code, font_size, line_pad, language):
     if language is not None:
         lexer = get_lexer_by_name(language)
@@ -55,7 +76,7 @@ def highlight_image(code, font_size, line_pad, language):
         lexer = guess_lexer(code)
 
     formatter = ImageFormatter(line_numbers=False, image_format='png',
-                               font_size=font_size, line_pad=line_pad, style=OneDarkStyle)
+                               font_size=font_size, line_pad=line_pad, style=OneHalfLightStyle)
     image_bytes = BytesIO(highlight(code, lexer, formatter))
     code_image = Image.open(image_bytes)
 
